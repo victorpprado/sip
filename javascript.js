e6eb858cfeb9a99f0a32b69f3f83f9a73a6af61a
Victor Prado <e.victorprado@gmail.com>
2024-04-26 17:07:15 -0300

    Improvements to Layout
    Still using bootstrap to work on layout

--- javascript.js
+++ javascript.js
@@ -1,11 +1,12 @@
 
 window.addEventListener('click', (e) => {
-     if (e.target.className == 'visibility-trigger') {
+     if (e.target.className == 'form-check-input visibility-trigger') {
         let temp = window.document.getElementById(e.target.id).parentNode.parentNode.parentNode.childNodes[7]
         let tempLabel = window.document.getElementById(e.target.id).nextSibling.nextSibling.innerText
         if (tempLabel == 'Interno') {
             temp.style.display = 'none'
         } else if (tempLabel == 'Externo') {
             temp.style.display = 'block'
+            window.document.getElementById(e.target.id).parentNode.parentNode.parentNode.childNodes[7].childNodes[1].childNodes[1].focus()
         }
 }})
