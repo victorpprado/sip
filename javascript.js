a18dfd5deb862dc281a4ad7263f18f527f81cdc5
Victor Prado <e.victorprado@gmail.com>
2024-04-27 18:01:20 -0300

    Improving the layout with bootstrap

--- javascript.js
+++ javascript.js
@@ -7,6 +7,25 @@ window.addEventListener('click', (e) => {
             temp.style.display = 'none'
         } else if (tempLabel == 'Externo') {
             temp.style.display = 'block'
-            window.document.getElementById(e.target.id).parentNode.parentNode.parentNode.childNodes[7].childNodes[1].childNodes[1].focus()
+            temp.childNodes[1].childNodes[1].focus()
         }
 }})
+
+window.addEventListener('click', (e) => {
+    if(e.target.id == 'presencial') {
+        window.document.querySelector('.hidden-room').style.display = 'block'
+        window.document.querySelector('#sala').style.display = 'block'
+        window.document.querySelector('.hidden-link').style.display = 'none'
+        window.document.querySelector('#link').style.display = 'none'
+    } else if (e.target.id == 'híbrida') {
+        window.document.querySelector('.hidden-room').style.display = 'block'
+        window.document.querySelector('#sala').style.display = 'block'
+        window.document.querySelector('.hidden-link').style.display = 'block'
+        window.document.querySelector('#link').style.display = 'block'
+    } else if (e.target.id == 'remota') {
+        window.document.querySelector('.hidden-room').style.display = 'none'
+        window.document.querySelector('#sala').style.display = 'none'
+        window.document.querySelector('.hidden-link').style.display = 'block'
+        window.document.querySelector('#link').style.display = 'block'
+    }
+})
